f34020c54047a29fb9b54294896d3b8c1f4c2528
Arnošt Havelka <arnost.havelka@gmail.com>
2015-04-29 15:43:52 +0200

    + example of secured resource

---
+++ src/main/java/com/github/aha/cert/servlet/RestrictedServlet.java
@@ -0,0 +1,24 @@
+package com.github.aha.cert.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/restricted/hello")
+public class RestrictedServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+
+		ServletOutputStream out = response.getOutputStream();
+		out.print("Here's the secret ...");
+	}
+}
